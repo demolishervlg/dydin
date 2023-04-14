ab79e9d256084d00adcafdb4bedd2a2a0932352e
demolishervlg <belykh.arthur@gmail.com>
2023-04-14 18:29:35 +0300

    Вёрстка с работы

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$(function(){
+
+
+// Выпадающее меню
+    $(document).ready(function(){
+        $('.menu-item').hover(function () {
+            clearTimeout($.data(this,'timer'));
+            $('ul',this).stop(true,true).slideDown(200);
+        }, function () {
+            $.data(this,'timer', setTimeout($.proxy(function() {
+                $('ul',this).stop(true,true).slideUp(200);
+            }, this), 100));
+        });
+    });
+
+
+
+})
