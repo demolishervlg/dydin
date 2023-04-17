970c8746effbacbaa0342f2120196e03149c1d4a
demolishervlg <belykh.arthur@gmail.com>
2023-04-17 18:17:06 +0300

    New

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
+// Выпадающее меню
+$(document).ready(function(){
+    $('.menu-item').hover(function () {
+        clearTimeout($.data(this,'timer'));
+        $('ul',this).stop(true,true).slideDown(150);
+    }, function () {
+        $.data(this,'timer', setTimeout($.proxy(function() {
+            $('ul',this).stop(true,true).slideUp(120);
+        }, this), 100));
+    });
+});
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "80%";
+}
+
+/* Установите ширину боковой панели в 0 (скройте ее) */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
 
 $(function(){
-
-
-// Выпадающее меню
-    $(document).ready(function(){
-        $('.menu-item').hover(function () {
-            clearTimeout($.data(this,'timer'));
-            $('ul',this).stop(true,true).slideDown(200);
-        }, function () {
-            $.data(this,'timer', setTimeout($.proxy(function() {
-                $('ul',this).stop(true,true).slideUp(200);
-            }, this), 100));
-        });
-    });
-
 
 
 })
